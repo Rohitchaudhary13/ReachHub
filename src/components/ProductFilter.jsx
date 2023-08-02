425caf18cff2253834c33c940a13c3baa66ec10f
Bloodsplash <abhinavchaudhary1318@gmail.com>
2023-08-02 17:12:56 +0530

    ShopCart Clone Completed

--- src/components/ProductFilter.jsx
+++ src/components/ProductFilter.jsx
@@ -62,41 +62,47 @@ const ProductFilter = ({ products, setFilteredProducts }) => {
   ];
 
   return (
-    <div className="mb-4 flex justify-between">
-        <div className='flex items-baseline gap-4'>
-      <label htmlFor="priceRange" className="block text-sm font-medium text-gray-700">
-        Price Range:
-      </label>
-      <div className="flex space-x-2 mb-2">
-        {priceRanges.map((range) => (
-          <button
-            key={range}
-            className={`border rounded-full py-1 focus:outline-none px-3 ${
-              selectedPriceRange === range
-                ? 'bg-[#003d2a] text-white'
-                : 'bg-gray-100 text-gray-700'
-            }`}
-            onClick={() => handleFilterChange(range)}
-          >
-            {range === '' ? 'All' : range}
-          </button>
-        ))}
+    <div className="mb-4 flex flex-col md:flex-row justify-between items-center">
+      {/* Price Range */}
+      <div className="mb-4 md:mb-0 flex items-center gap-4">
+        <label htmlFor="priceRange" className="block text-sm font-medium text-gray-700">
+          Filter By Price:
+        </label>
+        <div className="flex flex-wrap space-x-2">
+          {priceRanges.map((range) => (
+            <button
+              key={range}
+              className={`border rounded-full py-1 px-3 ${
+                selectedPriceRange === range
+                  ? 'bg-[#003d2a] text-white'
+                  : 'bg-gray-100 text-gray-700'
+              }`}
+              onClick={() => handleFilterChange(range)}
+            >
+              {range === '' ? 'All' : range}
+            </button>
+          ))}
+        </div>
       </div>
-      </div>
-    <div className='flex items-center gap-4 '>
-      <select
-        id="sortOption"
-        className="mt-1 rounded-full block w-full pl-3 pr-10 py-2 text-base bg-gray-100 border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-        value={selectedSortOption}
-        onChange={(e) => handleSortChange(e.target.value)}
-      >
-        <option value="">Sort By</option>
-        {sortOptions.map((option) => (
-          <option key={option.value} value={option.value}>
-            {option.label}
-          </option>
-        ))}
-      </select>
+
+      {/* Sort By */}
+      <div className="flex items-center mt-2 md:mt-0">
+        <label htmlFor="sortOption" className="block text-sm font-medium text-gray-700">
+          Sort By:
+        </label>
+        <select
+          id="sortOption"
+          className="mt-1 ml-2 rounded-full block w-full md:w-auto pl-3 pr-10 py-2 text-base bg-gray-100 border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+          value={selectedSortOption}
+          onChange={(e) => handleSortChange(e.target.value)}
+        >
+          <option value="">Sort By</option>
+          {sortOptions.map((option) => (
+            <option key={option.value} value={option.value}>
+              {option.label}
+            </option>
+          ))}
+        </select>
       </div>
     </div>
   );
